34e1999124cb4a6caefbe6a97bb22aaf0c8010da
Max B <bittmanmax@gmail.com>
2014-06-01 14:37:18 -0700

    Implementing sort ordering.

--- www/js/app.js
+++ www/js/app.js
@@ -51,155 +51,178 @@ config.templates = {
 };
 
 $(function() {
-  var handlers = function() {
-    $('.vote-icon-container .order-icon').off('click');
-    $('.vote-icon-container .order-icon').on('click', function(e) {
-      var id = $(this).data('id');
-      var val = parseInt($(this).data('val'), 10);
+  var serviceBrowser = sb = {
+
+    services: [],
+
+    loadServiceOrders: function() {
+      if (typeof localStorage.peoplesOpenServices !== "string") {
+        return {};
+      }
       try {
-        localStorage.peoplesOpenDotNet[id].order += val;
+        var serviceOrders = JSON.parse(localStorage.peoplesOpenServices);
+        return serviceOrders;
       } catch (e) {
-        if (typeof localStorage.peoplesOpenDotNet === "undefined") {
-          localStorage.peoplesOpenDotNet = {};
+        return {};
+      }
+    },
+
+    saveServiceOrders: function(serviceOrders) {
+      localStorage.peoplesOpenServices = JSON.stringify(serviceOrders);
+      _.each(serviceOrders, function(serviceOrder, id) {
+        var service = _.find(sb.services, function(s) {
+          return s.unique === id;
+        });
+        if (typeof service === "object") {
+          if (isNaN(parseInt(service.sortOrder, 10))) {
+            service.sortOrder = 0;
+          }
+          service.sortOrder = serviceOrder.order;
         }
-        if (typeof localStorage.peoplesOpenDotNet[id] === "undefined") {
-          localStorage.peoplesOpenDotNet[id] = {};
-        }
-        if (typeof localStorage.peoplesOpenDotNet[id].order === "undefined") {
-          localStorage.peoplesOpenDotNet[id].order = 0;
-        }
-      }
-    });
-  };
-
-  $('.services-container').html(config.templates.columnsTemplate(config.templateConfig));
-
-  var services = [];
-
-  var drawService = function(service) {
-    var column = _.size(services) % config.templateConfig.columnNum;
-    $('.services-container .column.col-' + column).append(service.html);
-  };
-
-  var drawServices = function() {
-    var column;
-    for (column = 0; column < config.templateConfig.columnNum; column++) {
-      $('.services-container .column.col-' + column).html('');
-    }
-    services = _.sortBy(services, 'sortOrder');
-    var i = 0;
-    _.each(services, function(service) {
-      var column = i % config.templateConfig.columnNum;
-      $('.services-container .column.col-' + i).append(service.html);
-      i++;
-    });
-    handlers();
-  };
-
-  function serviceUp(service) {
-    var containsService = _.find(services, function(serviceIter) {
-      return serviceIter.unique === service.unique;
-    });
-    if (service.txtRecord.scope === "peoplesopen.net" && !containsService) {
-      service.host = service.host.replace(/\.$/, '');
-
-      var link = false;
-      if(service.type.name === 'http') { 
-        link = {
-          url: 'http://'+service.host+':'+service.port+'/',
-          name: service.name
-        };
-      } else if(service.type.name === 'https') {
-        link = {
-          url: 'https://'+service.host+':'+service.port+'/',
-          name: service.name
-        };
-      } 
-
-      var serviceClass = undefined;
-
-      if (typeof service.txtRecord === 'object' &&
-          typeof service.txtRecord.type === 'string' && 
-          typeof config.icons === 'object' &&
-          typeof config.icons[service.txtRecord.type] === 'string') {
-        serviceClass = config.icons[service.txtRecord.type];
-      }
-
-        
-      var context = {
-        link: link,
-        name: service.name,
-        id: service.unique,
-        description: service.txtRecord.description,
-        serviceClass: serviceClass,
-        columnNum: config.columns
-      }
-
-      service.html = config.templates.serviceTemplate(context);
-
-      // Ok now we figure out where it should be added - we have to look at 
-      // whether or not it's been assigned an "order"
-      var id = service.unique;
-      if (typeof localStorage.peoplesOpenDotNet === "object" &&
-          typeof localStorage.peoplesOpenDotNet[id] === "object" &&
-          typeof localStorage.peoplesOpenDotNet[id].order !== "undefined") { // TODO: Add a better check for isNumber
-        
-        service.sortOrder = localStorage.peoplesOpenDotNet[id].order;
-      } else {
-        service.sortOrder = 0;
-      }
-
-      services.push(service);
-      //drawService(service);
-      drawServices();
-
-    }
-  }
-
-  function serviceDown(service) {
-    if (typeof service.uniqueId === 'string' &&
-        services[service.uniqueId] === 'object') {
-
-      services = _.filter(services, function(serviceIter) {
-        return serviceIter.uniqueId !== service.uniquId;
       });
-    }
-    var removeNode = $('.service-box[data-id="' + service.unique + '"]');
-    if (removeNode.length > 0 ) {
-      removeNode.remove();
-    }
-  }
+    },
 
-  // INIT CODE:
-  if (Modernizr.localstorage) {
-    localStorage.peoplesOpenDotNet = {};
-  } else {
-    console.log("no local storage - probably will not work entirely");
-  }
+    handlers: function() {
+      $('.vote-icon-container .order-icon').off('click');
+      $('.vote-icon-container .order-icon').on('click', function(e) {
+        var id = $(this).data('id');
+        var val = parseInt($(this).data('val'), 10);
+        var serviceOrders = sb.loadServiceOrders();
+        if (typeof serviceOrders[id] !== "object") {
+          serviceOrders[id] = {};
+        }
+        if (typeof serviceOrders[id].order === "undefined") {
+          serviceOrders[id].order = 0;
+        }
+        serviceOrders[id].order += val;
+        sb.saveServiceOrders(serviceOrders);
+        sb.drawServices();
+      });
+    },
 
-  //var sock = new SockJS.create(config.thisUrl + '/websocket');
-  var sock = new SockJS('/websocket');
-
-  sock.onopen = function() {
-    console.log('open');
-  };
-
-  sock.onmessage = function(e) {
-    console.log(e);
-    var data = JSON.parse(e.data);;
-    console.log(data);
-    if(data.type == 'service') {
-
-      if(data.action == 'up') {
-        serviceUp(data.service);
-      } else if(data.action == 'down') {
-        serviceDown(data.service);
+    drawServices: function() {
+      var column;
+      for (column = 0; column < config.templateConfig.columnNum; column++) {
+        $('.services-container .column.col-' + column).html('');
       }
+      sb.services = _.sortBy(sb.services, function(s) {
+        return -s.sortOrder;
+      });
+      var i = 0;
+      _.each(sb.services, function(service) {
+  
+        var context = {
+          link: service.link,
+          name: service.name,
+          id: service.unique,
+          description: service.txtRecord.description,
+          serviceClass: service.serviceClass,
+          columnNum: config.columns,
+          sortOrder: service.sortOrder
+        };
+
+        service.html = config.templates.serviceTemplate(context);
+
+        var column = i % config.templateConfig.columnNum;
+        $('.services-container .column.col-' + column).append(service.html);
+        i++;
+      });
+      sb.handlers();
+    },
+
+    serviceUp: function(service) {
+      var containsService = _.find(sb.services, function(serviceIter) {
+        return serviceIter.unique === service.unique;
+      });
+      if (service.txtRecord.scope === "peoplesopen.net" && !containsService) {
+        service.host = service.host.replace(/\.$/, '');
+
+        service.link = false;
+        if(service.type.name === 'http') { 
+          service.link = {
+            url: 'http://'+service.host+':'+service.port+'/',
+            name: service.name
+          };
+        } else if(service.type.name === 'https') {
+          service.link = {
+            url: 'https://'+service.host+':'+service.port+'/',
+            name: service.name
+          };
+        } 
+
+        if (typeof service.txtRecord === 'object' &&
+            typeof service.txtRecord.type === 'string' && 
+            typeof config.icons === 'object' &&
+            typeof config.icons[service.txtRecord.type] === 'string') {
+          service.serviceClass = config.icons[service.txtRecord.type];
+        }
+
+        // Ok now we figure out where it should be added - we have to look at 
+        // whether or not it's been assigned an "order"
+        var id = service.unique;
+        var serviceOrders = sb.loadServiceOrders();
+        if (typeof serviceOrders[id] === "object" &&
+            typeof serviceOrders[id] !== "undefined") { // TODO: Add a better check for isNumber
+          service.sortOrder = serviceOrders[id].order;
+        } else {
+          service.sortOrder = 0;
+        }
+        sb.services.push(service);
+        sb.drawServices();
+
+      }
+    },
+
+    serviceDown: function(service) {
+      if (typeof service.uniqueId === 'string' &&
+          sb.services[service.uniqueId] === 'object') {
+
+        sb.services = _.filter(sb.services, function(serviceIter) {
+          return serviceIter.uniqueId !== service.uniquId;
+        });
+      }
+      var removeNode = $('.service-box[data-id="' + service.unique + '"]');
+      if (removeNode.length > 0 ) {
+        removeNode.remove();
+      }
+    },
+
+    init: function() {
+      // INIT CODE:
+      if (Modernizr.localstorage) {
+        localStorage.peoplesOpenDotNet = {};
+      } else {
+        console.log("no local storage - probably will not work entirely");
+      }
+      $('.services-container').html(config.templates.columnsTemplate(config.templateConfig));
+
+      //var sock = new SockJS.create(config.thisUrl + '/websocket');
+      var sock = new SockJS('/websocket');
+
+      sock.onopen = function() {
+        console.log('open');
+      };
+
+      sock.onmessage = function(e) {
+        console.log(e);
+        var data = JSON.parse(e.data);;
+        console.log(data);
+        if(data.type === 'service') {
+          if(data.action === 'up') {
+            sb.serviceUp(data.service);
+          } else if(data.action === 'down') {
+            sb.serviceDown(data.service);
+          }
+        }
+      };
+
+      sock.onclose = function() {
+        console.log('close');
+      };  
+
     }
   };
 
-  sock.onclose = function() {
-    console.log('close');
-  };  
+  serviceBrowser.init();
 
 });
